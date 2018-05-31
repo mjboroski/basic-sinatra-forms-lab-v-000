33b5e61911304d33e7f8080d612b1edbb234d12c
Michael Boroski <mjboroski@gmail.com>
2018-05-31 22:55:11 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,9 +7,14 @@ class App < Sinatra::Base
   end
 
   post '/newteam' do
-
-    erb :team
-  end
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @pf = params[:pf]
+    @sf = params[:sf]
+    @c = params[:c]
+    erb :team  end
 
 
 end
